49b1a1a4d06502c46ef7da774a3a4628b1b123ac
Haoyu Chen <null-qwerty@outlook.com>
2025-03-03 06:16:27 +0800

    ci(Core): CubeMX MCU pakage upgrade

--- Core/Inc/stm32h7xx_it.h
+++ Core/Inc/stm32h7xx_it.h
@@ -22,7 +22,7 @@
 #define __STM32H7xx_IT_H
 
 #ifdef __cplusplus
- extern "C" {
+extern "C" {
 #endif
 
 /* Private includes ----------------------------------------------------------*/
@@ -62,6 +62,7 @@ void DMA1_Stream6_IRQHandler(void);
 void SPI2_IRQHandler(void);
 void USART2_IRQHandler(void);
 void USART3_IRQHandler(void);
+void UART5_IRQHandler(void);
 void TIM23_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
