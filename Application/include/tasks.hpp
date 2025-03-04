48f698bba514f1701ce5eb92ca7911ae8f90020e
Haoyu Chen <null-qwerty@outlook.com>
2025-03-04 20:32:12 +0800

    feat: now we can motor wheels :)

--- Application/include/tasks.hpp
+++ Application/include/tasks.hpp
@@ -1,12 +1,10 @@
 #pragma once
 
 #include "main.h"
-#include "FreeRTOS.h"
-#include "cmsis_os2.h"
-#include "task.h"
 #include "semphr.h"
 
 #include "BaseControl/Connectivity/UART/DBUS.hpp"
+#include "BaseControl/Motor/RM3508.hpp"
 
 void WheelLegTasksInit(void);
 
@@ -19,8 +17,34 @@ void WheelLegTasksInit(void);
  */
 __weak void vTaskReadDbus(void *pvParameters);
 
+/**
+ * @brief 接收轮毂的电机的反馈信号
+ *
+ * @param pvParameters 任务参数，未使用
+ *
+ * @note 以 1000Hz 的频率接收电机的反馈信号，将反馈值存入全局变量中
+ * (osPriorityAboveNormal)
+ */
+__weak void vTaskWheelReceive(void *pvParameters);
+
+/**
+ * @brief 向轮毂的电机发送控制信号
+ *
+ * @param pvParameters 任务参数，未使用
+ *
+ * @note 通过 notify 执行 (osPriorityAboveNormal)
+ */
+__weak void vTaskWheelControl(void *pvParameters);
+
 extern xTaskHandle readDbusTaskHandle;
+
+extern xTaskHandle wheelReceiveTaskHandle;
+extern xTaskHandle wheelControlTaskHandle;
 
 extern SemaphoreHandle_t wheelControlMutex;
 
 extern DBUS dbus;
+
+extern FDCAN_FilterTypeDef wheelFdcanFilter;
+extern FDCAN wheelConnectivity;
+extern RM3508 leftWheel, rightWheel;
