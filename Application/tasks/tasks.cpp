a69eeff6aef59a339561c40293dfc0ecdb943b15
Haoyu Chen <null-qwerty@outlook.com>
2025-03-22 22:03:33 +0800

    feat(Task): add joint task & LED task

--- Application/tasks/tasks.cpp
+++ Application/tasks/tasks.cpp
@@ -5,11 +5,19 @@
 void WheelLegTasksInit(void)
 {
     /* 创建任务 */
+    xTaskCreate(vTaskLed, "ledTask", 128 * 2, NULL, osPriorityNormal,
+                &ledTaskHandle);
     xTaskCreate(vTaskReadDbus, "readDbusTask", 128 * 2, NULL,
                 osPriorityAboveNormal, &readDbusTaskHandle);
     xTaskCreate(vTaskWheelReceive, "wheelReceiveTask", 128 * 2, NULL,
                 osPriorityAboveNormal, &wheelReceiveTaskHandle);
     xTaskCreate(vTaskWheelControl, "wheelControlTask", 128 * 2, NULL,
                 osPriorityAboveNormal, &wheelControlTaskHandle);
+    xTaskCreate(vTaskJointInit, "jointInitTask", 128 * 2, NULL,
+                osPriorityAboveNormal, &jointInitTaskHandle);
+    xTaskCreate(vTaskLeftJointTransmit, "jointTransmitTask", 128 * 2, NULL,
+                osPriorityAboveNormal, &lJointTransmitTaskHandle);
+    xTaskCreate(vTaskRightJointTransmit, "jointTransmitTask", 128 * 2, NULL,
+                osPriorityAboveNormal, &rJointTransmitTaskHandle);
     return;
 }
