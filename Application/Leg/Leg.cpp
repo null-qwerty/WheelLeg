9bbebc7bb9dca41a46850e2699bc7024754c616b
Haoyu Chen <null-qwerty@outlook.com>
2025-07-03 22:37:42 +0800

    refactor(Leg): refactor existing code

--- Application/Leg/Leg.cpp
+++ Application/Leg/Leg.cpp
@@ -1,7 +1,8 @@
-#include "Leg.hpp"
+#include "Leg/Leg.hpp"
 
 #include "Math/Matrix.hpp"
 #include "Math/Trigonometric.hpp"
+#include "Math/Vector.hpp"
 
 Leg::Leg(Joint &frontJoint, Joint &backJoint, Wheel &wheel)
     : frontJoint(frontJoint)
@@ -12,16 +13,14 @@ Leg::Leg(Joint &frontJoint, Joint &backJoint, Wheel &wheel)
 
 Leg &Leg::init()
 {
-    /**
-     * TODO 初始化腿部关节电机
-     *      1. 撞击限位，设置软零点
-     *      2. 将关节电机设置到水平位置
-     */
+    // TODO 初始化腿部关节电机
+    //  1. 撞击限位，设置软零点
+    //  2. 将关节电机设置到水平位置
 
     return *this;
 }
 
-Leg &Leg::setParam(float l1, float l2, float l3, float l4, float l5)
+Leg &Leg::setLegLenth(float l1, float l2, float l3, float l4, float l5)
 {
     this->l1 = l1;
     this->l2 = l2;
@@ -37,6 +36,13 @@ Leg &Leg::setParam(float l1, float l2, float l3, float l4, float l5)
     return *this;
 }
 
+Leg &Leg::setLinearzationParam(Matrix<12, 6> p)
+{
+    pk = p;
+
+    return *this;
+}
+
 Leg &Leg::setJointLimit(float maxAngle, float minAngle)
 {
     angleLimitMax = maxAngle;
@@ -45,23 +51,43 @@ Leg &Leg::setJointLimit(float maxAngle, float minAngle)
     return *this;
 }
 
-Leg &Leg::setVirtualTorque(float vertical, float horizontal)
+Leg::State &Leg::getCurrentState()
+{
+    return current_state;
+}
+
+Leg::State &Leg::getTargetState()
+{
+    return target_state;
+}
+
+Leg &Leg::calculateTotalTorque()
+{
+    X = Vector6f((float *)(&target_state)) -
+        Vector6f((float *)(&current_state)); // 计算当前状态与目标状态的差值向量
+    u_balance = calculateK() * X; // 计算为了保持平衡状态的虚拟力矩
+    // u_balance[0]: 动力轮力矩
+    // u_balance[1]: 摆杆力矩
+
+    // TODO: 添加控制腿长的力矩计算；留接口用于外部传入防止劈叉的控制力矩
+    // 合成力矩
+    u[0] = 0; // 径向力矩
+    u[1] = u_balance[1]; // 切向力矩
+    transformToJointTorque(u); // 将合成力矩转换为关节力矩
+
+    frontJoint.getTargetState().toreque = u[0]; // 设置前关节的目标力矩
+    backJoint.getTargetState().toreque = u[1];
+
+    return *this;
+}
+
+Leg &Leg::transformToJointTorque(Vector2f &u)
 {
     calculateCurrentState();
 
-    auto &phi0 = state.phi0;
-    auto &l0 = state.l0;
-    // \begin{align}
-    // T(t) &= \begin{bmatrix}
-    // \frac{l_1\Math::sin(\phi_0-\phi_3)\Math::sin(\phi_1-\phi_2)}{\Math::sin(\phi_3-\phi_2)}
-    // &
-    // \frac{l_1\Math::cos(\phi_0-\phi_3)\Math::sin(\phi_1-\phi_2)}{l_0\Math::sin(\phi_3-\phi_2)}
-    // \newline
-    // \frac{l_4\Math::sin(\phi_0-\phi_2)\Math::sin(\phi_3-\phi_4)}{\Math::sin(\phi_3-\phi_2)}
-    // &
-    // \frac{l_4\Math::cos(\phi_0-\phi_2)\Math::sin(\phi_3-\phi_4)}{l_0\Math::sin(\phi_3-\phi_2)}
-    // \end{bmatrix}
-    // \end{align}
+    auto &phi0 = current_state.phi;
+    auto &l0 = current_state.l;
+
     Matrix2x2f T;
     // clang-format off
     T << l1 * Math::sin(phi1 - phi3) * Math::sin(phi0 - phi2) / Math::sin(phi3 - phi2),
@@ -69,61 +95,46 @@ Leg &Leg::setVirtualTorque(float vertical, float horizontal)
          l4 * Math::sin(phi0 - phi2) * Math::sin(phi3 - phi4) / Math::sin(phi3 - phi2),
          l4 * Math::cos(phi0 - phi2) * Math::sin(phi3 - phi4) / (l0 * Math::sin(phi3 - phi2));
     // clang-format on
-    Vector2f q, t;
-    q << vertical, horizontal;
-    t = T * q;
-
-    frontJoint.getTargetState().toreque = t[0];
-    backJoint.getTargetState().toreque = t[1];
+    u = T * u;
 
     return *this;
-}
-
-Leg &Leg::setWheelTorque(float torque)
-{
-    wheel.getTargetState().toreque = torque;
-
-    return *this;
-}
-
-Leg::State &Leg::getState()
-{
-    calculateCurrentState();
-
-    return state;
 }
 
 Leg &Leg::calculateCurrentState()
 {
     phi1 = PI - frontJoint.getState().position;
     phi4 = backJoint.getState().position;
-    // x_B = x_A + l_1 * Math::cos(phi_1);
-    // y_B = y_A + l_1 * Math::sin(phi_1);
-    // x_D = x_E + l_4 * Math::cos(phi_4);
-    // y_D = y_E + l_4 * Math::sin(phi_4);
+
     B[0] = A[0] + l1 * Math::cos(phi1);
     B[1] = A[1] + l1 * Math::sin(phi1);
     D[0] = E[0] + l4 * Math::cos(phi4);
     D[1] = E[1] + l4 * Math::sin(phi4);
-    // l_BD_2 = (x_B - x_D) * (x_B - x_D) + (y_B - y_D) * (y_B - y_D);
-    // m = 2l_2(x_B - x_D)
-    // n = 2l_2(y_B - y_D)
-    // p = l_3_2 - l_2_2 - l_BD_2
-    // phi2 = 2 * atan2(n + sqrt(m*m + n*n - p*p), m + p);
-    // phi3 = 2 * atan2(n - sqrt(m*m + n*n - p*p), m + p);
+
     auto vBD = B - D;
     auto lBD2 = vBD.dot(vBD);
     auto m = 2 * l2 * vBD[0];
     auto n = 2 * l2 * vBD[1];
     auto p = l3 * l3 - l2 * l2 - lBD2;
+
     phi2 = 2 * Math::atan2(n + sqrt(m * m + n * n - p * p), m + p);
     phi3 = 2 * Math::atan2(n - sqrt(m * m + n * n - p * p), m + p);
-    // x_dot_C = diff(x_C, t);
-    // y_dot_C = diff(y_C, t);
+
     C[0] = B[0] + l2 * Math::cos(phi2);
     C[1] = B[1] + l2 * Math::sin(phi2);
-    state.phi0 = Math::atan2(C[1], C[0]);
-    state.l0 = C.magnitude();
+
+    current_state.phi = Math::atan2(C[1], C[0]);
+    current_state.l = C.magnitude();
 
     return *this;
+}
+
+Matrix<2, 6> &Leg::calculateK()
+{
+    auto &l = current_state.l;
+    Vector6f l0;
+    l0 << l * l * l * l * l, l * l * l * l, l * l * l, l * l, l, 1;
+    auto tempK = pk * l0; // 线性拟合腿长为 l 的增益矩阵
+    K = (float *)(tempK);
+
+    return this->K;
 }
